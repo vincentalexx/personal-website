cbbf62055e23a0b863e2d748886c0dbf8f66a18f
vincent vincent <vincentalexx03.com>
2023-09-30 11:42:05 +0700

    navbar done%

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
                 
                 <a href="/" className="md:ml-20 ml-10 text-white text-3xl -mt-2">Vincentalexx</a>
 
-                <div onClick={() => setisOpen(!isOpen)} className="text-gray-500 right-16 w-7 h-7 absolute cursor-pointer lg:hidden text-2xl"> 
+                <div onClick={() => setisOpen(!isOpen)} className="text-gray-500 right-16 w-7 h-7 absolute cursor-pointer lg:hidden text-2xl transition-all duration-300 ease-out"> 
                     {
                         isOpen ? <FontAwesomeIcon icon={faXmark}/> :  <FontAwesomeIcon icon={faBars} />
                     }
                 </div>
 
-                <ul className={'text-gray-500 lg:space-x-7 lg:flex pl-9 justify-between items-center lg:py-0 py-4 right-16 absolute lg:z-auto space-y-3 lg:right-10 lg:space-y-0 bg-gray-800/[0.95] lg:bg-gray-800/[1] lg:border-0 lg:shadow-none shadow-xl border-2 border-gray-600 lg:top-8 top-[87px] lg:rounded-none rounded-md transition-all duration-300 ease-out'}>
+                <ul className={'text-gray-500 lg:space-x-7 hidden lg:flex pl-9 justify-between items-center lg:py-0 py-4 right-16 absolute lg:z-auto space-y-3 lg:right-10 lg:space-y-0 bg-gray-800/[0.95] lg:bg-gray-800/[1] lg:border-0 lg:shadow-none shadow-xl border-2 border-gray-600 lg:top-8 top-[87px] lg:rounded-none rounded-md transition-all duration-300 ease-out'}>
                     
                     <li className="hover:text-white transition-all duration-300">
                         <a href="#home" className="home-link"><FontAwesomeIcon icon={faHome} className="mr-3 fa-xl" />Home</a>
@@ -32,6 +32,24 @@ const Navbar = () => {
                         <a href="#contact" className="hover:text-white transition-all duration-300 mr-20"><FontAwesomeIcon icon={faIdCardClip} className="mr-3 fa-xl" />Contact</a>
                     </li>
                 </ul>
+
+                {isOpen && (
+                    <ul className={'text-gray-500 lg:space-x-7 lg:flex pl-9 justify-between items-center lg:py-0 py-4 right-16 absolute lg:z-auto space-y-3 lg:right-10 lg:space-y-0 bg-gray-800/[0.95] lg:bg-gray-800/[1] lg:border-0 lg:shadow-none shadow-xl border-2 border-gray-600 lg:top-8 top-[87px] lg:rounded-none rounded-md transition-all duration-300 ease-out'}>
+                        
+                        <li className="hover:text-white transition-all duration-300">
+                            <a href="#home" className="home-link"><FontAwesomeIcon icon={faHome} className="mr-3 fa-xl" />Home</a>
+                        </li>
+                        <li>
+                            <a href="#about" className="hover:text-white transition-all duration-300"><FontAwesomeIcon icon={faUser} className="mr-3 fa-xl ml-[3px]" />About</a>
+                        </li>
+                        <li>
+                            <a href="#portfolio" className="hover:text-white transition-all duration-300"><FontAwesomeIcon icon={faBookBookmark} className="mr-3 fa-xl ml-[3px]" />Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#contact" className="hover:text-white transition-all duration-300 mr-20"><FontAwesomeIcon icon={faIdCardClip} className="mr-3 fa-xl" />Contact</a>
+                        </li>
+                    </ul>
+                )}
             </div>
         </section>
     );
