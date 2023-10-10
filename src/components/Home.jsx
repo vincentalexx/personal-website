ca9e0429ac5f7d1e492014156c887f00c6459655
vincentalexx <vincentalexx03@gmail.com>
2023-10-11 01:22:43 +0700

    revisi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
     return (
         <section id="home">
             <div className='home'>
-                <div className='h-36 lg:h-32'></div>
+                <div className='h-44 lg:h-36'></div>
 
-                <div className='justify-center mx-auto md:flex md:space-x-16 xl:space-x-28 space-y-8 md:space-y-0 w-[80vw] md:mt-[80px] lg:mt-[110px] h-[80vh] md:h-[70vh] lg:h-[68vh]'>
+                <div className='justify-center mx-auto md:flex md:space-x-16 xl:space-x-28 space-y-8 md:space-y-0 mt-[70px] md:mt-[80px] lg:mt-[110px] min-h-[90vh] md:min-h-[70vh] lg:min-h-[75vh]'>
                     <div className=''>
-                        <img src={image} alt='photo' className="drop-shadow-[3px_3px_white] object-cover aspect-square max-h-[350px] mx-auto min-w-[250px]" />
+                        <img src={image} alt='photo' className="drop-shadow-[3px_3px_white] object-cover aspect-square max-h-[300px] md:max-h-[350px] lg:max-h-[450px] mx-auto min-w-[250px]" />
                     </div>
                     <div className='justify-center flex items-center text-center content-center max-w-xl mx-auto align-middle md:max-h-[280px] lg:max-h-[350px]'>
-                        <p className='text-white font-bold mx-auto max-w-md md:w-[380px] lg:w-[448px] text-2xl md:text-3xl lg:text-5xl'>
+                        <p className='text-white font-bold mx-auto max-w-md md:w-[380px] lg:w-[448px] text-2xl md:text-3xl lg:text-5xl mt-24'>
                             <Typewriter options={{strings: Title, autoStart: true, loop: true,}} />
                         </p>
                     </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,9 +7,9 @@ const Portfolio =() => {
     return (
         <section id="portfolio">
             <div className='h-24 lg:h-20'></div>
-            <div className='mb-40'>
+            <div className='mb-40 min-h-[70vh]'>
                 <span className='flex text-3xl justify-center  md:mt-[50px] lg:mt-[70px] mb-11'>PORTFOLIO</span>
-                <div className='gap-y-12 grid-cols-1 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 py-4 md:mx-16 lg:mx-20 cursor-pointer'>
+                <div className='gap-y-12 grid-cols-1 grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 py-4 md:mx-16 lg:mx-20 cursor-pointer'>
                     {PortfolioList.map((portfolio) => {
                         return <PortfolioItem name={portfolio.name} image={portfolio.image} link={portfolio.link} />
                     })}
